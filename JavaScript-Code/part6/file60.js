66e51c8e0eab280d50e053cb88cbb923dc8cf47b
Shrinath0209 <shri02092005@gmail.com>
2026-02-18 14:28:14 +0530

    Updated

---
+++ JavaScript-Code/part6/file60.js
@@ -0,0 +1,12 @@
+// const cart = {
+//     items:[],
+//     addItems(items){
+//        this.items.push(x)
+//     }
+//     showItems:(){
+//         console.log(this.items)
+//     }
+// }
+// cart.addItems("Laptop")
+// cart.addItems("Desktop")
+// cart.showItems()
