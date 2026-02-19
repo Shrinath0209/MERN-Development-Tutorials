1a8231b18facf4396cda3f28546ccba2ce07bb7e
Shrinath0209 <shri02092005@gmail.com>
2026-02-19 11:54:38 +0530

    Updated

--- NodeJs/index3.js
+++ NodeJs/index3.js
@@ -13,3 +13,11 @@ app.get("/Home",(req,res)=>{
     res.send("responded from server / Home")
 
 })
+app.get("/Home/page1",(req,res)=>{
+    console.log(req.url)
+    console.log(req.method)
+    console.log(req.body)
+    console.log(req.headers.authorization)
+    res.send("responded from server / Home/page1")
+
+})
